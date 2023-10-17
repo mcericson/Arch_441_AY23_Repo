e1a615183f3fb73e8e3e8a39f89bd3eeccceb764
Mark Ericson <ericson.mark.c@gmail.com>
2023-10-16 21:09:59 -0700

    update to tutorial

--- 10_17/image_analysis_tutorial.py
+++ 10_17/image_analysis_tutorial.py
@@ -1,8 +1,9 @@
 #mark ericson
 #10/17/2023
-#This program translates images into geometry
+#This program translates images into geometry.
 
-#imports----------------------------------
+
+#imports------------------------------------
 
 import rhinoscriptsyntax as rs
 import System.Drawing.Bitmap as Bitmap
@@ -14,13 +15,14 @@ from imp import reload
 reload(bt)
 
 
-#definitions------------------------------
+#definitions---------------------------------
 
 def assign_material_color(object, color):
     rh_color = rs.CreateColor(color)
     rs.AddMaterialToObject(object)
     index = rs.ObjectMaterialIndex(object)
     rs.MaterialColor(index, rh_color)
+    rs.ObjectColor(object, rh_color)
 
 def image_to_coordinates(file_path, resolution):
     img = Bitmap.FromFile(file_path)
@@ -31,63 +33,58 @@ def image_to_coordinates(file_path, resolution):
     w_step = int(width/resolution)
     h_step = int(height/resolution)
     
-    colors = []
-    locations = []
-    
     pixel_dim = w_step, h_step
     
+    colors = []
+    locations = []
     for i in range(0, width, w_step):
-        x = i
         for j in range(0, height, h_step):
+            x = i
             y = j
-            r,g,b,a = img.GetPixel(x, y)
             location = (x, y, 0)
+            r, g, b, a = img.GetPixel(x, y)
             locations.append(location)
-            colors.append((r,g,b,a))
-    nl = locations.reverse()
+            colors.append((r, g, b, a))
+    locations.reverse()
+
     return locations, colors, pixel_dim
 
-def image_to_geometry(image_path, resolution, geometry='points'):
-
+def image_to_geometry(image_path, resolution, geometry = 'point'):
     rs.EnableRedraw(False)
     image_data = image_to_coordinates(image_path, resolution)
     
     points = image_data[0]
     colors = image_data[1]
-    w, h = image_data[2]
+    w, l = image_data[2]
+    
     for i in range(len(points)):
-        if geometry == 'points':
+        if geometry == 'point':
             point = rs.AddPoint(points[i])
-            color = rs.CreateColor(colors[i])
-            rs.ObjectColor(point, color)
+            assign_material_color(point, colors[i])
         if geometry == 'box':
-            height_value = colors[i][0]*10
-            box = bt.base_box(points[i], height_value, w, h)
+            r, g, b, a  = colors[i]
+            box = bt.base_box(points[i], b+ 10 , w, l)
             assign_material_color(box, colors[i])
         if geometry == 'line':
-            r,g,b,a = colors[i]
+            r, g, b, a = colors[i]
             x, y, z = points[i]
             point_a = (x, y, z)
             point_b = (x + r, y + g, z + b)
             line = rs.AddLine(point_a, point_b)
-            color = rs.CreateColor(r,g,b,a)
-            rs.ObjectColor(line, color)
+            assign_material_color(line, colors[i])
+            
 
-    rs.EnableRedraw(True)
 
-#main---------------------------------------------
-
+#main---------------------------------------------------
 
 def main():
-   
-    vt.create_parallel_view('new_top', (1000,1000))
-    image_path = "web_mountains_small.jpg"
-    image_to_geometry(image_path, 100, 'line')
+    view_name = 'new_top'
+    vt.create_parallel_view(view_name,(1000,1000))
+    vt.set_axon_view(45, 120, view_name)
+    file_path = 'web_mountains_small.jpg'
+    image_to_geometry(file_path, 50, 'line')
     rs.ZoomExtents()
-    vt.capture_view(2.0, 'tst_image', 'test_folder')
+    vt.capture_view(2.0, 'line_test', 'line_tests')
 
 
 main()
-
-
-
