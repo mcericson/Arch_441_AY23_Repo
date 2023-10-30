35ec537aa3c37d1da73932f17596d1648b496fa8
Mark Ericson <ericson.mark.c@gmail.com>
2023-10-30 15:17:26 -0700

    updates

--- 10_24/image_analysis_tutorial.py
+++ 10_24/image_analysis_tutorial.py
@@ -16,29 +16,15 @@ reload(bt)
 
 
 #definitions---------------------------------
-def grid(x_num, y_num, space):
-    points = []
-    for i in range(0, x_num * space, space):
-        for j in range(0, y_num * space, space):
-            x = i
-            y = j
-            point = (x, y, 0)
-            points.append(point)
-    return points
 
+def point_attractor(original_point, attractor_point, magnitude):
+    new_point = rs.AddPoint(attractor_point)
+    vector_1 = rs.VectorCreate(new_point, original_point)
+    vector_2 = rs.VectorUnitize(vector_1)
+    vector_3 = rs.VectorScale(vector_2, magnitude)
+    moved_point = rs.MoveObject(original_point, vector_3)
+    return moved_point
 
-
-def is_point_inside(center, radius, point):
-    x, y, z = point
-    cx, cy, cz = center
-    
-    x_square  = (x-cx) * (x - cx)
-    y_square  = (y-cy) * (x - cx)
-    r_square = radius * radius
-    if (x_square + y_square) <= r_square:
-        return True
-    else:
-        return False
 
 def assign_material_color(object, color):
     rh_color = rs.CreateColor(color)
@@ -79,23 +65,18 @@ def image_to_geometry(image_path, resolution, geometry = 'point'):
     points = image_data[0]
     colors = image_data[1]
     w, l = image_data[2]
-    points_grid = grid(4, 4, 50)
-    for point in points_grid:
-        rs.AddCircle(point, 20)
-  
+    attractor_point = rs.GetPoint()
     for i in range(len(points)):
         if geometry == 'point':
             point = rs.AddPoint(points[i])
             assign_material_color(point, colors[i])
         if geometry == 'box':
             r, g, b, a  = colors[i]
-            for j in points_grid:
-                point_inside = is_point_inside(j, 20, points[i])
-                if point_inside == False:
-                    box = bt.base_box(points[i], b+ 10 , w, l)
-                    assign_material_color(box, colors[i])
-                else:
-                    pass
+            point_a = rs.AddPoint(points[i])
+            point_b = point_attractor(point_a, attractor_point, 1000)
+            point_c = rs.PointCoordinates(point_b)
+            box = bt.base_box(point_c, b+ 10 , w, l)
+            assign_material_color(box, colors[i])
         if geometry == 'line':
             r, g, b, a = colors[i]
             x, y, z = points[i]
@@ -103,19 +84,23 @@ def image_to_geometry(image_path, resolution, geometry = 'point'):
             point_b = (x + r, y + g, z + b)
             line = rs.AddLine(point_a, point_b)
             assign_material_color(line, colors[i])
-
-
+        if geometry == 'point_attractor':
+            point_a = rs.AddPoint(points[i])
+            point_b = point_attractor(point_a, attractor_point, 1000)
+            np = rs.AddPoint(point_b)
+            assign_material_color(point_b, colors[i])
 
 #main---------------------------------------------------
 
 def main():
     view_name = 'new_top'
-    vt.create_parallel_view(view_name,(1000,1000))
-    vt.set_axon_view(45, 120, view_name)
+
     file_path = 'web_mountains_small.jpg'
     image_to_geometry(file_path, 50, 'box')
-    rs.ZoomExtents()
-    #vt.capture_view(2.0, 'line_test', 'line_tests')
+#    vt.create_parallel_view(view_name,(1000,1000))
+#    vt.set_axon_view(45, 120, view_name)
+#    rs.ZoomExtents()
+#    vt.capture_view(2.0, 'line_test', 'line_tests')
 
 
 main()
