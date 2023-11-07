9f703450585bc3dcfb14284fbae7ef3f84cfea62
Mark Ericson <ericson.mark.c@gmail.com>
2023-11-07 09:59:57 -0800

    Update chunk_list.py

--- 11_07/project_folder/chunk_list.py
+++ 11_07/project_folder/chunk_list.py
@@ -1,13 +1,10 @@
 def chunk_list(items, size):
     return [items[i:i + size] for i in range(0 ,len(items), size)]
 
-def flip_matrix(matrix):
-    return [ for list() in zip(*matrix)]
-
 list = [1,2,3,4,5,6,7,8]
 
 new_list = (chunk_list(list, 2))
-nl = flip_matrix(new_list)
+
 print new_list
-print (nl)
+
 
